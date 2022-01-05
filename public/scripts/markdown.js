ada4e08395042df661a998e238fdf93777f4c2ea
Jake Freeland <jakesfreeland@gmail.com>
2022-01-05 17:00:54 -0600

    Added webAssembly markdown parser

---
+++ public/scripts/markdown.js
@@ -0,0 +1,13 @@
+import * as markdown from "/scripts/markdown-wasm/markdown.es.js";
+import DOMPurify from '/scripts/dompurify/purify.es.js';
+await markdown.ready
+
+const bodyMarkDown = document.getElementById("body-md");
+const bodyHTML = document.getElementById("body-html");
+const bodyPreview = document.getElementById("body-preview");
+
+bodyMarkDown.addEventListener("input", () => {
+  let html = DOMPurify.sanitize(markdown.parse(bodyMarkDown.value));
+  bodyHTML.textContent = html;
+  bodyPreview.innerHTML = html;
+})
