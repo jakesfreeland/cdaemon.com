538262c246a62a435d6a7e643a9e25647c529f43
Jake Freeland <jakesfreeland@gmail.com>
2022-01-12 22:06:33 -0600

    started /tags/:tag

--- routes/tags.js
+++ routes/tags.js
@@ -6,11 +6,21 @@ const db = require("../user_modules/db.cjs");
 // create table in database tags for respective tag
 router.route("/:tag")
 .get((req, res) => {
-  db.getData("tags", `${req.params.tag}`)
-  .then(tags => {
-    res.render("tags/tag", { tags: tags })
-  })
-  .catch(console.log);
+  getTaggedPosts(req.params.tag)
+  .then(posts => res.render("tags/tag", { posts: posts }))
+  .catch(console.log)
 })
+
+async function getTaggedPosts(tag) {
+  const pids = await db.getData("blog_tags", tag);
+  let posts = [];
+
+  for (var i=0; i<pids.length; ++i) {
+    const post = await db.getValueData("blog_posts", "post", "pid", pids[i].pid);
+    posts.push(post[0]);
+  }
+
+  return posts;
+}
 
 module.exports = router;
