d0a023da9dfb6ac011c6448df69f76c197b2301e
Jake Freeland <jfree@FreeBSD.org>
2023-09-08 12:40:00 -0500

    Revert "posts: Enable syntax highlighting for code blocks"
    Revert commit with hash 6ce896b17f0e336028cc2fa7a8ae056a1e9c2ab2.
    
    Syntax highlighting puts 30% load on my server's CPU for a single page
    refresh. This is not scalable, so just remove it.

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,6 @@ const path = require("path");
 const { JSDOM } = require('jsdom');
 const createDOMPurify = require('dompurify');
 const markdown = require("markdown-wasm");
-const hljs = require('highlight.js/lib/common');
 const db = require("../user_modules/db.cjs");
 
 const window = new JSDOM('').window;
@@ -125,12 +124,7 @@ router.route("/:pid")
     res.render("posts/post", {
       title: post[0].title,
       /* sanitization to mitigate XSS */
-      body: DOMPurify.sanitize(
-        markdown.parse(post[0].body, {
-          onCodeBlock(lang, body) {
-            return hljs.highlightAuto(body.toString()).value;
-          },
-        })),
+      body: DOMPurify.sanitize(markdown.parse(post[0].body)),
       tags: tags,
       banner: post[0].banner,
       author: JSON.parse(post[0].author),
